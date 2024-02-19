0c3b8b45b627dba1924129b09ad702c903903693
Rafa Kraetzer <73673435+Rafa-GitHub@users.noreply.github.com>
2024-02-19 19:39:40 -0300

    SiteAulaEntra21

---
+++ siteMentoria/jsConceitos/script.js
@@ -0,0 +1,31 @@
+/*
+function calcular(){
+    let valor01 = Number(document.getElementById("primeiroValor").value)
+    let valor02 = Number(document.getElementById("segundoValor").value)
+    const option = document.querySelector("input[name='option']:checked").value
+    
+    if((valor01 >= 0 && valor01 <= 100) && (valor02 >= 0 && valor02 <= 100)){
+        switch(option){
+            case "+":
+                document.getElementById("resultado").innerHTML = valor01 + valor02
+                break
+            case "-":
+                document.getElementById("resultado").innerHTML = valor01 - valor02
+                break
+            case "/":
+                document.getElementById("resultado").innerHTML = valor01 / valor02
+                break
+            case "*":
+                document.getElementById("resultado").innerHTML = valor01 * valor02
+                break
+        }
+    }else{
+        alert("Digite valores validos!")
+    }
+
+}
+*/
+
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
